c9aaa39ad9bed0e090b863c46e68c42f30e7ccc4
gregorvwk <69797933+gregorvwk@users.noreply.github.com>
2020-08-28 13:43:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+const superheroes = [
+    {name: "Batman", alter_ego: "Bruce Wayne"}, 
+    {name: "Superman", alter_ego: "Clark Kent"}, 
+    {name: "Spiderman", alter_ego: "Peter Parker"}]
+
+    // let findSpiderMan = superheroes.find((array) => {
+    //     array.name === "Spiderman"
+    // })
+    // console.log(findSpiderMan); 
+
+    // console.log(superheroes.find === name, "Spiderman")
+
+    // let findSpiderMan = function(array){
+    //     return array.find("spiderman")
+    // }
+    // console.log(findSpiderMan(superheroes));
+
+    // let findSpiderMan = function(name){
+    //     return name.find === name, "Spiderman";
+    // }
+    // console.log(findSpiderMan(superheroes));
+
+    // let findSpiderMan = function(superHeroes){
+    //     let i = superheroes.indexOf === "Spiderman";
+    //     return i;
+    // }
+    // console.log(superheroes[findSpiderMan]);
+
+    // let findSpiderMan = function(array){
+    //     return array.find("spiderman", name)
+    
+    // }
+    //     console.log(findSpiderMan(superheroes));
+    // let findSpiderMan = function(array) {
+    //     return array.name.find === "Spiderman";
+    // }
+    // console.log(findSpiderMan(superheroes));
+
+    // let findSpiderMan = (array => {
+    //     array.find === name;
+    //     console.log(name);
+    // })
+    // console.log(findSpiderMan);
+    // console.log(findSpiderMan(superHeroes));
+
+    // console.log(findSpiderMan(superHeroes)); 
+    // Find Spiderman
+    // result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
